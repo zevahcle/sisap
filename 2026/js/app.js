5cffb7f77d61313b26b772f956cb9de006646dc8
VladimirMic <xmic@fi.muni.cz>
2026-07-18 22:59:15 +0200

    Update app.js

--- 2026/js/app.js
+++ 2026/js/app.js
@@ -5,19 +5,19 @@ $(document).ready(function () {
         openOpacity: false,
         closeBtn: false,
         tpl: {
-            next: '<a title="' + texts.data('next') + '" class="fancybox-nav fancybox-next"><span></span></a>',
-            prev: '<a title="' + texts.data('previous') + '" class="fancybox-nav fancybox-prev"><span></span></a>',
+            next: '<a title="Next" class="fancybox-nav fancybox-next"><span></span></a>',
+            prev: '<a title="Previous" class="fancybox-nav fancybox-prev"><span></span></a>',
         },
         helpers: {
             title: {
                 type: 'over' // 'float', 'inside', 'outside' or 'over'
             },
             buttons: {
-                tpl: '<div id="fancybox-buttons"><ul><li><a class="btnPrev" title="' + texts.data('previous') + '" href="javascript:;"></a></li><li><a class="btnPlay" title="' + texts.data('start_slideshow') + '" href="javascript:;"></a></li><li><a class="btnNext" title="' + texts.data('next') + '" href="javascript:;"></a></li><li><a class="btnToggle" title="' + texts.data('toggle_size') + '" href="javascript:;"></a></li><li><a class="btnClose" title="' + texts.data('close') + '" href="javascript:;"></a></li></ul></div>'
+                tpl: '<div id="fancybox-buttons"><ul><li><a class="btnPrev" title="Previous" href="javascript:;"></a></li><li><a class="btnPlay" title="Start slideshow" href="javascript:;"></a></li><li><a class="btnNext" title="' + texts.data('next') + '" href="javascript:;"></a></li><li><a class="btnToggle" title="' + texts.data('toggle_size') + '" href="javascript:;"></a></li><li><a class="btnClose" title="Close" href="javascript:;"></a></li></ul></div>'
             }
         },
         onPlayStart: function () {
-            $(".btnPlay").attr('title', texts.data('pause_slideshow'));
+            $(".btnPlay").attr('title', "Pause slideshow");
         }
     });
 }
